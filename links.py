49ef21108dc2ab864dd9fef513b019af1b08f97c
Jerome Belleman <jerome.belleman@gmail.com>
2017-07-17 19:12:59 +0200

    Pylint

--- links.py
+++ links.py
@@ -5,7 +5,7 @@ def tweet(account, inline=True):
 
     if not inline:
         print '<p>'
-    print '<a href="https://twitter.com/share" class="twitter-share-button" data-via="%s" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>' % account
+    print '<a href="https://twitter.com/share" class="twitter-share-button" data-via="%s" data-show-count="false">Tweet</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>' % account # pylint: disable=line-too-long
     if not inline:
         print '</p>'
 
@@ -16,6 +16,6 @@ def follow(account, inline=True):
 
     if not inline:
         print '<p>'
-    print '<a href="https://twitter.com/%s" class="twitter-follow-button" data-show-count="false">Follow @%s</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>' % (account, account)
+    print '<a href="https://twitter.com/%s" class="twitter-follow-button" data-show-count="false">Follow @%s</a><script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>' % (account, account) # pylint: disable=line-too-long
     if not inline:
         print '</p>'
